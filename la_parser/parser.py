d5c58096c2742b49e9d87c8319bbe61f00353716
Yotam Gingold <yotam@yotamgingold.com>
2020-04-24 15:47:02 -0400

    cache the parser, which is slow to create.

--- la_parser/parser.py
+++ la_parser/parser.py
@@ -41,14 +41,41 @@ def walk_model(parser_type, model):
         walker = NumpyWalker()
     return walker.walk_model(model)
 
+def create_parser():
+    grammar = open('la_grammar/LA.ebnf').read()
+    parser = tatsu.compile(grammar, asmodel=True)
+    return parser
+
+_last_parser = None
+_last_parser_mtime = None
+def get_parser():
+    '''
+    Equivalent to create_parser(), but stores the result for immediate access in the future.
+    Re-creates the parser if any grammar files change.
+    '''
+    global _last_parser, _last_parser_mtime
+    ## Collect all .ebnf file modification times.
+    from pathlib import Path
+    import os
+    # print( 'grammar paths:', [ str(f) for f in Path('la_grammar').glob('*.ebnf') ] )
+    mtimes = [ os.path.getmtime(path) for path in Path('la_grammar').glob('*.ebnf') ]
+    # print( 'mtimes:', mtimes )
+    ## If the parser hasn't been created or a file has changed, re-create it.
+    if _last_parser is None or any([ t >= _last_parser_mtime for t in mtimes ]):
+        print( "Creating parser..." )
+        start_time = time.time()
+        _last_parser = create_parser()
+        _last_parser_mtime = time.time()
+        print("------------ %.2f seconds ------------" % (time.time() - start_time))
+    
+    return _last_parser
 
 def parse_and_translate(content):
     # try:
     start_time = time.time()
-    grammar = open('la_grammar/LA.ebnf').read()
-    parser_type = ParserTypeEnum.NUMPY
-    parser = tatsu.compile(grammar, asmodel=True)
+    parser = get_parser()
     model = parser.parse(content, parseinfo=True)
+    parser_type = ParserTypeEnum.NUMPY
     res = walk_model(parser_type, model)
     result = (res, 0)
     print("------------ %.2f seconds ------------" % (time.time() - start_time))
