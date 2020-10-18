95cc04abab21390b5d5f67babd3db5bb68bb82bb
pressureless <190361902@qq.com>
2020-10-18 19:00:12 -0400

    Use cache dir rather than project dir

--- la_tools/parser_manager.py
+++ la_tools/parser_manager.py
@@ -8,7 +8,9 @@ from os import listdir
 from pathlib import Path
 import hashlib
 import importlib
+import importlib.util
 import threading
+import shutil
 MAXIMUM_SIZE = 12  # 10 + 2 default
 
 
@@ -17,13 +19,16 @@ class ParserManager(object):
         self.logger = LaLogger.getInstance().get_logger(LoggerTypeEnum.DEFAULT)
         self.parser_dict = {}
         self.prefix = "grammar"
-        self.module_dir = "la_local_parsers"
+        self.module_dir = "iheartla"
         self.default_parsers = [hashlib.md5("init".encode()).hexdigest(), hashlib.md5("default".encode()).hexdigest()]
         # create the user's cache directory (pickle)
-        # self.cache_dir = user_cache_dir("iheartla")
-        # dir_path = Path(self.cache_dir)
-        # if not dir_path.exists():
-        #     dir_path.mkdir()
+        self.cache_dir = user_cache_dir(self.module_dir)
+        dir_path = Path(self.cache_dir)
+        if not dir_path.exists():
+            dir_path.mkdir()
+            # copy default parsers
+            for f in listdir("la_local_parsers"):
+                shutil.copy("la_local_parsers/"+f, self.cache_dir)
         # self.cache_file = Path(self.cache_dir + '/parsers.pickle')
         #######
         self.load_parsers()
@@ -38,12 +43,15 @@ class ParserManager(object):
                 print("IO error:{}".format(e))
 
     def load_parsers(self):
-        for f in listdir(self.module_dir):
+        for f in listdir(self.cache_dir):
             if self.prefix in f and '.py' in f and '_' in f:
                 name = f.split('.')[0]
                 hash_value = name.split('_')[1]
-                module_name = "la_local_parsers.{}".format(name)
-                module = importlib.import_module(module_name)
+                module_name = "{}.{}".format(self.module_dir, name)
+                path_to_file = "{}/{}.py".format(self.cache_dir, name)
+                spec = importlib.util.spec_from_file_location(module_name, path_to_file)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
                 parser_a = getattr(module, "grammar{}Parser".format(hash_value))
                 parser_semantic = getattr(module, "grammar{}ModelBuilderSemantics".format(hash_value))
                 parser = parser_a(semantics=parser_semantic())
@@ -74,7 +82,7 @@ class ParserManager(object):
         code_model = tatsu.to_python_model(grammar, name="grammar{}".format(hash_value), filename='la_grammar/here')
         code_model = code_model.replace("from __future__ import print_function, division, absolute_import, unicode_literals", "")
         code += code_model
-        save_to_file(code, "{}/grammar_{}.py".format("la_local_parsers", hash_value))
+        save_to_file(code, "{}/grammar_{}.py".format(self.cache_dir, hash_value))
 
     def check_parser_cnt(self):
         parser_size = len(self.parser_dict)
@@ -83,22 +91,23 @@ class ParserManager(object):
             earliest_time = time.time()
             earliest_file = None
             earliest_hash = None
-            for f in listdir(self.module_dir):
+            for f in listdir(self.cache_dir):
                 if self.prefix in f and '.py' in f and '_' in f:
                     name = f.split('.')[0]
                     hash_value = name.split('_')[1]
                     if hash_value not in self.default_parsers:
-                        cur_time = os.path.getmtime(self.module_dir + '/' + f)
+                        cur_time = os.path.getmtime(self.cache_dir + '/' + f)
                         if cur_time < earliest_time:
                             earliest_time = cur_time
                             earliest_file = f
                             earliest_hash = hash_value
             if earliest_file is not None and earliest_hash in self.parser_dict:
                 del self.parser_dict[earliest_hash]
-                os.remove(self.module_dir + '/' + earliest_file)
+                os.remove(self.cache_dir + '/' + earliest_file)
                 parser_size = len(self.parser_dict)
             else:
-                 break
+                # avoid dead loop
+                break
         self.logger.debug("check_parser_cnt, self.parser_dict:{}".format(self.parser_dict))
 
     def save_dict(self):
