6d7eacaea10503d979d717fe8fc6807847b70287
pressureless <190361902@qq.com>
2021-03-31 19:02:29 -0400

    Init outdent func

--- web/src-noconflict/mode-iheartla.js
+++ web/src-noconflict/mode-iheartla.js
@@ -63,6 +63,47 @@ ace.define("ace/mode/iheartla_highlight_rules",["require","exports","module","ac
     exports.IheartlaHighlightRules = IheartlaHighlightRules;
 });
 
+ace.define("ace/mode/matching_brace_outdent",["require","exports","module","ace/lib/oop","ace/range"], function (require, exports, module) {
+"use strict";
+
+var Range = require("../range").Range;
+
+var MatchingBraceOutdent = function() {};
+
+(function() {
+
+    this.checkOutdent = function(line, input) {
+        if (! /^\s+$/.test(line))
+            return false;
+
+        return /^\s*\}/.test(input);
+    };
+
+    this.autoOutdent = function(doc, row) {
+        var line = doc.getLine(row);
+        var match = line.match(/^(\s*\})/);
+
+        if (!match) return 0;
+
+        var column = match[1].length;
+        var openBracePos = doc.findMatchingBracket({row: row, column: column});
+
+        if (!openBracePos || openBracePos.row == row) return 0;
+
+        var indent = this.$getIndent(doc.getLine(openBracePos.row));
+        doc.replace(new Range(row, 0, row, column-1), indent);
+    };
+
+    this.$getIndent = function(line) {
+        return line.match(/^\s*/)[0];
+    };
+
+}).call(MatchingBraceOutdent.prototype);
+
+exports.MatchingBraceOutdent = MatchingBraceOutdent;
+});
+
+
 ace.define("ace/mode/folding/ihla",["require","exports","module","ace/lib/oop","ace/range","ace/mode/folding/fold_mode"], function(require, exports, module) {
 "use strict";
 
@@ -172,11 +213,13 @@ ace.define("ace/mode/iheartla",["require","exports","module","ace/lib/oop","ace/
     var TextMode = require("./text").Mode;
     var IheartlaHighlightRules = require("./iheartla_highlight_rules").IheartlaHighlightRules;
     var FoldMode = require("./folding/ihla").FoldMode;
+    var MatchingBraceOutdent = require("./matching_brace_outdent").MatchingBraceOutdent;
 
     var Mode = function () {
         this.HighlightRules = IheartlaHighlightRules;
         this.foldingRules = new FoldMode();
         this.$behaviour = this.$defaultBehaviour;
+        this.$outdent = new MatchingBraceOutdent();
     };
     oop.inherits(Mode, TextMode);
 
@@ -185,6 +228,19 @@ ace.define("ace/mode/iheartla",["require","exports","module","ace/lib/oop","ace/
         
         this.$id = "ace/mode/iheartla";
         this.snippetFileId = "ace/snippets/iheartla";
+
+        this.getNextLineIndent = function(state, line, tab) {
+            var indent = this.$getIndent(line);
+            return indent;
+        };
+
+        this.checkOutdent = function(state, line, input) {
+            return this.$outdent.checkOutdent(line, input);
+        };
+
+        this.autoOutdent = function(state, doc, row) {
+            this.$outdent.autoOutdent(doc, row);
+        };
     }).call(Mode.prototype);
 
     exports.Mode = Mode;
