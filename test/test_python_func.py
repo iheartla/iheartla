69b96a7c45ff40607dbd45eda9052d327f03e237
pressureless <190361902@qq.com>
2021-04-13 21:30:59 -0400

    Add func tests

--- test/test_python_func.py
+++ test/test_python_func.py
@@ -11,8 +11,8 @@ cppyy.add_include_path(eigen_path)
 class TestFunction(BasePythonTest):
     def test_func_no_param(self):
         la_str = """A = Pf()P(P)
-                    where 
-                    P: ℝ ^ (2 × 2): a matrix 
+                    where
+                    P: ℝ ^ (2 × 2): a matrix
                     f: {} -> ℝ^(2 × 2): a function"""
         func_info = self.gen_func_info(la_str)
         P = np.array([[1, 2], [4, 3]])
@@ -39,8 +39,8 @@ class TestFunction(BasePythonTest):
 
     def test_func_param_types_matrix(self):
         la_str = """A = Pf(P)
-                    where 
-                    P: ℝ ^ (2 × 2): a matrix 
+                    where
+                    P: ℝ ^ (2 × 2): a matrix
                     f: ℝ^(2 × 2) -> ℝ^(2 × 2): a function """
         func_info = self.gen_func_info(la_str)
         P = np.array([[1, 2], [4, 3]])
@@ -66,9 +66,9 @@ class TestFunction(BasePythonTest):
 
     def test_func_param_types_vector(self):
         la_str = """A = Q f(P)
-                    where 
-                    P: ℝ ^ 2: a matrix 
-                    Q: ℝ ^(2×2): a matrix 
+                    where
+                    P: ℝ ^ 2: a matrix
+                    Q: ℝ ^(2×2): a matrix
                     f: ℝ^(2) -> ℝ^(2 × 2): a function"""
         func_info = self.gen_func_info(la_str)
         P = np.array([1, 4])
@@ -101,8 +101,8 @@ class TestFunction(BasePythonTest):
 
     def test_func_param_types_scalar(self):
         la_str = """A = P f(2)
-                    where 
-                    P: ℝ ^(2×2): a matrix 
+                    where
+                    P: ℝ ^(2×2): a matrix
                     f: scalar -> ℝ^(2 × 2): a function"""
         func_info = self.gen_func_info(la_str)
         P = np.array([[1, 2], [4, 3]])
@@ -134,8 +134,8 @@ class TestFunction(BasePythonTest):
 
     def test_func_multi_params(self):
         la_str = """A = P f(2, 3)
-                    where 
-                    P: ℝ ^(2×2): a matrix 
+                    where
+                    P: ℝ ^(2×2): a matrix
                     f: scalar, scalar -> ℝ^(2 × 2): a function"""
         func_info = self.gen_func_info(la_str)
         P = np.array([[1, 2], [4, 3]])
@@ -167,8 +167,8 @@ class TestFunction(BasePythonTest):
 
     def test_func_ret_types_vector(self):
         la_str = """A = P f(2)
-                    where 
-                    P: ℝ ^(2×2): a matrix 
+                    where
+                    P: ℝ ^(2×2): a matrix
                     f: scalar -> ℝ^(2 ): a function"""
         func_info = self.gen_func_info(la_str)
         P = np.array([[1, 2], [4, 3]])
@@ -199,8 +199,8 @@ class TestFunction(BasePythonTest):
 
     def test_func_ret_types_scalar(self):
         la_str = """A = P f(2)
-                    where 
-                    P: ℝ ^(2×2): a matrix 
+                    where
+                    P: ℝ ^(2×2): a matrix
                     f: scalar -> ℤ: a function"""
         func_info = self.gen_func_info(la_str)
         P = np.array([[1, 2], [4, 3]])
@@ -226,8 +226,8 @@ class TestFunction(BasePythonTest):
 
     def test_nested_func(self):
         la_str = """A = Pf(g()P)P
-                    where 
-                    P: ℝ ^ (2 × 2): a matrix 
+                    where
+                    P: ℝ ^ (2 × 2): a matrix
                     f: ℝ^(2 × 2) -> ℝ^(2 × 2): a function
                     g: {} -> ℝ^(2 × 2): a function """
         func_info = self.gen_func_info(la_str)
@@ -260,8 +260,8 @@ class TestFunction(BasePythonTest):
 
     def test_recursive_func(self):
         la_str = """A = Pf(P+f(f(P⋅P))P)
-                    where 
-                    P: ℝ ^ (2 × 2): a matrix 
+                    where
+                    P: ℝ ^ (2 × 2): a matrix
                     f: ℝ^(2 × 2) -> ℝ^(2 × 2): a function"""
         func_info = self.gen_func_info(la_str)
         P = np.array([[1, 2], [4, 3]])
@@ -281,6 +281,91 @@ class TestFunction(BasePythonTest):
                      "    };",
                      "    Eigen::Matrix<double, 2, 2> B = {}(P, f).A;".format(func_info.eig_func_name),
                      "    return ((B - A).norm() == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_func_assign(self):
+        la_str = """a = p
+                    c = a(t)p(2t)
+                    where 
+                    p ∈ ℝ -> ℝ
+                    t ∈ ℝ """
+        func_info = self.gen_func_info(la_str)
+        f = lambda p : 2*p
+        self.assertEqual(func_info.numpy_func(f, 1).c, 8)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    std::function<double(double)> f;"
+                     "    f = [](double p)->double{",
+                     "    return 2*p;"
+                     "    };",
+                     "    double B = {}(f, 1).c;".format(func_info.eig_func_name),
+                     "    return (B == 8);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_seq_func_assign(self):
+        la_str = """a_i = p_i
+                    c_i = a_i(1)
+                    where 
+                    p_i ∈ ℝ -> ℝ"""
+        func_info = self.gen_func_info(la_str)
+        P = np.array([1, 2])
+        f = lambda p : p
+        f1 = lambda p : 2*p
+        self.assertDMatrixEqual(func_info.numpy_func(np.asarray([f, f1])).c, P)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    std::vector<std::function<double(double)> > f_i(2);"
+                     "    std::function<double(double)> f;"
+                     "    f = [](double p)->double{",
+                     "    return p;"
+                     "    };",
+                     "    std::function<double(double)> f2;"
+                     "    f2 = [](double p)->double{",
+                     "    return 2*p;"
+                     "    };",
+                     "    f_i[0] = f;",
+                     "    f_i[1] = f2;",
+                     "    std::vector<double > B = {}(f_i).c;".format(func_info.eig_func_name),
+                     "    return (B[0] == 1 and B[1] == 2);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_seq_func_sum(self):
+        la_str = """c = sum_i p_i(A_i)
+                    where 
+                    p_i ∈ ℝ -> ℝ
+                    A_i ∈ ℝ"""
+        func_info = self.gen_func_info(la_str)
+        A = np.array([1, 2])
+        f = lambda p : p
+        f1 = lambda p : 2*p
+        self.assertEqual(func_info.numpy_func(np.asarray([f, f1]), A).c, 5)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    std::vector<double> A(2);"
+                     "    A[0] = 1;"
+                     "    A[1] = 2;"
+                     "    std::vector<std::function<double(double)> > f_i(2);"
+                     "    std::function<double(double)> f;"
+                     "    f = [](double p)->double{",
+                     "    return p;"
+                     "    };",
+                     "    std::function<double(double)> f2;"
+                     "    f2 = [](double p)->double{",
+                     "    return 2*p;"
+                     "    };",
+                     "    f_i[0] = f;",
+                     "    f_i[1] = f2;",
+                     "    double B = {}(f_i, A).c;".format(func_info.eig_func_name),
+                     "    return B == 5;",
                      "}"]
         cppyy.cppdef('\n'.join(func_list))
         self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
