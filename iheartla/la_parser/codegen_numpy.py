531eb58e1783a7e2aea2675efc0f2e893ad8389f
pressureless <190361902@qq.com>
2021-06-02 22:25:38 -0400

    Check set size (#46)

--- iheartla/la_parser/codegen_numpy.py
+++ iheartla/la_parser/codegen_numpy.py
@@ -371,8 +371,8 @@ class CodeGenNumpy(CodeGen):
             elif self.symtable[parameter].is_set():
                 type_declare.append('    {} = frozenset({})'.format(parameter, parameter))
                 # type_checks.append('    assert isinstance({}, list) and len({}) > 0'.format(parameter, parameter))
-                # if self.symtable[parameter].size > 1:
-                #     type_checks.append('    assert len({}[0]) == {}'.format(parameter, self.symtable[parameter].size))
+                if self.symtable[parameter].size > 1:
+                    type_checks.append('    assert all( len(el) == {} for el in {} )'.format(self.symtable[parameter].size, parameter))
                 test_content += self.get_set_test_list(parameter, self.generate_var_name("dim"), 'i', self.symtable[parameter],
                                                        rand_int_max, '    ')
             elif self.symtable[parameter].is_function():
