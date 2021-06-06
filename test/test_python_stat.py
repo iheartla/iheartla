4641eca58afd8ba67a9e40fec052dac312836942
pressureless <190361902@qq.com>
2021-06-05 23:23:27 -0400

    Add test

--- test/test_python_stat.py
+++ test/test_python_stat.py
@@ -46,3 +46,25 @@ class TestStat(BasePythonTest):
                      "}"]
         cppyy.cppdef('\n'.join(func_list))
         self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_interleaved_stat(self):
+        la_str = """A: ℝ^(n×n)
+        a = A_1,1
+        n: ℤ"""
+        func_info = self.gen_func_info(la_str)
+        A = np.array([[2, 5], [0, 12]])
+        self.assertEqual(func_info.numpy_func(A, 2).a, 2)
+        # MATLAB test
+        if TEST_MATLAB:
+            mat_func = getattr(mat_engine, func_info.mat_func_name, None)
+            self.assertEqual(np.array(mat_func(matlab.double(A.tolist()), 2)['a']), 2)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    Eigen::Matrix<double, 2, 2> A;",
+                     "    A << 2, 5, 0, 12;",
+                     "    double B = {}(A, 2).a;".format(func_info.eig_func_name),
+                     "    return (B == 2);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
