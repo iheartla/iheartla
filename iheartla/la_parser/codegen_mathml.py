fe852b1016a9062b8009ee85cef1c64dc96af1bb
pressureless <190361902@qq.com>
2021-11-10 22:48:46 -0500

    Read sym info

--- iheartla/la_parser/codegen_mathml.py
+++ iheartla/la_parser/codegen_mathml.py
@@ -52,8 +52,8 @@ class CodeGenMathML(CodeGen):
             for subs in node.subs:
                 subs_list.append(self.convert_unicode(subs))
             return self.convert_unicode(node.main_id) + '_{' + ','.join(subs_list) + '}'
-        return r"""<mi data-mjx-variant="-tex-mathit" mathvariant="italic" onclick="event.stopPropagation(); alert('You clicked a symbol');" id='{}'>{}</mi>"""\
-            .format(self.convert_unicode(node.get_name()), self.convert_unicode(node.get_name()))
+        return r"""<mi data-mjx-variant="-tex-mathit" mathvariant="italic" onclick="event.stopPropagation(); onClickSymbol('{}','{}')" id='{}'>{}</mi>"""\
+            .format(self.convert_unicode(node.get_name()), self.func_name, self.convert_unicode(node.get_name()), self.convert_unicode(node.get_name()))
         # return self.convert_unicode(node.get_name())
 
     def visit_MatrixVdots(self, node, **kwargs):
