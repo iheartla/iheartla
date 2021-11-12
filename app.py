acbc6465334805874411344fea005c502605d9d2
pressureless <190361902@qq.com>
2021-11-11 23:50:23 -0500

    Add tag property

--- app.py
+++ app.py
@@ -93,6 +93,9 @@ function getSymInfo(symbol, func_name){
 function onClickSymbol(symbol, func_name) {
   console.log("clicked: " + symbol + " in " + func_name);
   alert(getSymInfo(symbol, func_name));
+  $( "mi[sym='C']" ).attr('color', 'blue');
+  const matches = document.querySelectorAll("mi[sym='C']");
+  console.log(matches);
 };
 function onClickEq(func_name, sym_list) { 
   content = "This equation has " + sym_list.length + " symbols\n";
@@ -108,7 +111,7 @@ function onClickEq(func_name, sym_list) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script type="text/javascript" id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-    <link rel="stylesheet" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <script>
 const iheartla_data = JSON.parse('{json}');
