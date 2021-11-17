4fccb9403a60a6119999b66182c59036de25adc7
pressureless <190361902@qq.com>
2021-11-17 15:36:15 -0500

    Init arrow function

--- extras/resource/paper.js
+++ extras/resource/paper.js
@@ -1,3 +1,48 @@
+function drawArrow( startElement, endElement, style='' ) {
+    // This pseudocode creates an SVG element for each "arrow". As an alternative,
+    // we could always have one SVG element present in the document
+    // with absolute position 0,0 (or along the right side of the window)
+    // whose width and height allows us to draw anywhere.
+    // Then we would add and remove child nodes as needed.
+    // You can use vanilla JS or svg.js or snap.js. Probably svg.js is a good fit.
+    // Create a new SVG node
+    // var svg = SVG().addTo('body').size('100%', '100%').move(-1010, -410);
+    var svg = SVG().addTo('body').size('100%', '100%');
+    var body = document.querySelector("body");
+    var style = body.currentStyle || window.getComputedStyle(body);
+    // Name it for selector queries so you can style it and also delete it
+    svg.addClass("arrow")
+    // Get the position of the start and end elements in absolute coordinates
+    console.log(startElement);
+    console.log(endElement);
+    var bodyRect = body.getBoundingClientRect();
+    var startRect = startElement.getBoundingClientRect();
+    var startCenterX = startRect.x + startRect.width/2;
+    var startCenterY = startRect.y + startRect.height/2;
+    var endRect = endElement.getBoundingClientRect();
+    var endCenterX = endRect.x + endRect.width/2;
+    var endCenterY = endRect.y + endRect.height/2;
+    var marginLeft = parseInt(style.marginLeft, 10)
+    var bodyWidth = parseInt(style.width, 10)
+    var marginTop = parseInt(style.marginTop, 10)
+    console.log(style); 
+    var arrowSize = 10;
+    var offset = 10;
+    var endPointX = endCenterX - bodyRect.x + marginLeft;
+    var endPointY = endCenterY - bodyRect.y + marginTop;
+    // var endPointX = bodyWidth+marginLeft-offset;
+    // var endPointY = endCenterY - bodyRect.y + marginTop;
+    // svg.path(`M${bodyWidth+marginLeft-offset} ${startCenterY - bodyRect.y + marginTop} 
+    svg.path(`M${startCenterX - bodyRect.x + marginLeft} ${startCenterY - bodyRect.y + marginTop} 
+      L ${bodyWidth+marginLeft} ${startCenterY - bodyRect.y + marginTop} 
+      L ${bodyWidth+marginLeft} ${endCenterY - bodyRect.y + marginTop} 
+      L ${endPointX} ${endPointY} 
+      L ${endPointX+arrowSize} ${endPointY-arrowSize} 
+      L ${endPointX} ${endPointY} 
+      L ${endPointX+arrowSize} ${endPointY+arrowSize} 
+      `).attr({fill: 'white', 'fill-opacity': 0, stroke: 'blue', 'stroke-width': 1})
+}
+
 function getSymTypeInfo(type_info){
   if(type_info.type == 'matrix'){
     content = "matrix, rows: " + type_info.rows + ", cols: " + type_info.cols;
@@ -93,6 +138,7 @@ function getSymInfo(symbol, func_name){
 }
 function onClickSymbol(tag, symbol, func_name) {
   closeOtherTips();
+  drawArrow(document.querySelector("#ff"), document.querySelector("#ss"));
   if (typeof tag._tippy === 'undefined'){
     tippy(tag, {
         content: getSymInfo(symbol, func_name),
@@ -106,6 +152,7 @@ function onClickSymbol(tag, symbol, func_name) {
           return true;  
         },
         onHide(instance) {
+          removeArrows();
           tag.removeAttribute('class');
           console.log('onHide');
           return true;  
@@ -140,7 +187,14 @@ function onClickEq(tag, func_name, sym_list) {
       });
   }
 };
+function removeArrows(){
+  var arrow = document.querySelector(".arrow");
+  if (arrow) {
+    document.querySelector("body").removeChild(arrow);
+  }
+}
 function closeOtherTips(){
+  removeArrows();
   const matches = document.querySelectorAll(".highlight");
   for (var i = matches.length - 1; i >= 0; i--) {
     matches[i].removeAttribute('class');
