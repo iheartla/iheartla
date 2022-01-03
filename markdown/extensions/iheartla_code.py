5525dd25e216f8b255a04bd92d3356e66e142240
pressureless <190361902@qq.com>
2022-01-03 13:57:09 -0500

    Avoid repeated replacing

--- markdown/extensions/iheartla_code.py
+++ markdown/extensions/iheartla_code.py
@@ -150,7 +150,7 @@ class IheartlaBlockPreprocessor(Preprocessor):
             for sym in sym_list:
                 PROSE_RE = re.compile(
                     dedent(r'''(?<!(    # negative begins
-                    (\\(proselabel|prosedeflabel)({{([a-z\p{{Ll}}\p{{Lu}}\p{{Lo}}\p{{M}}\s]+)}})?{{(\s*)))
+                    (\\(proselabel|prosedeflabel)({{([a-z\p{{Ll}}\p{{Lu}}\p{{Lo}}\p{{M}}\s]+)}})?{{([a-z\p{{Ll}}\p{{Lu}}\p{{Lo}}\p{{M}}_{{()\s]*)))
                     |
                     ([a-z\p{{Ll}}\p{{Lu}}\p{{Lo}}\p{{M}}\\])
                     ) # negative ends
