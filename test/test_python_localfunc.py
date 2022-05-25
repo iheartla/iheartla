e7c78ada163ce6b44552f9e7bb24c28ca807a020
pressureless <190361902@qq.com>
2022-05-24 20:52:09 -0400

    More local func tests

--- test/test_python_localfunc.py
+++ test/test_python_localfunc.py
@@ -25,6 +25,23 @@ class TestLocalFunction(BasePythonTest):
         cppyy.cppdef('\n'.join(func_list))
         self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
 
+    def test_local_func_where_position(self):
+        la_str = """f(x) = x 
+                    where x: ℝ ^ (2 × 2)"""
+        func_info = self.gen_func_info(la_str)
+        A = np.array([[209, 208], [416, 417]])
+        self.assertDMatrixEqual(func_info.numpy_func().f(A), A)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    Eigen::Matrix<double, 2, 2> A;",
+                     "    A << 209, 208, 416, 417;",
+                     "    Eigen::Matrix<double, 2, 2> B = {}().f(A);".format(func_info.eig_func_name),
+                     "    return ((B - A).norm() == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
     def test_local_func_same_param(self):
         la_str = """f(x) = x where x: ℝ ^ (2 × 2)
                     where
@@ -70,6 +87,53 @@ class TestLocalFunction(BasePythonTest):
                      "    A[1] = 2;"
                      "    double B = {}().f(A);".format(func_info.eig_func_name),
                      "    return ((B - 1) == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_local_func_use_global_param(self):
+        la_str = """f(x) = x + y where x: ℝ
+                    where
+                    y: ℝ : a scalar"""
+        func_info = self.gen_func_info(la_str)
+        self.assertEqual(func_info.numpy_func(2).f(3), 5)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    double B = {}(2).f(3);".format(func_info.eig_func_name),
+                     "    return ((B - 5) == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_local_func_use_defined_sym(self):
+        la_str = """z = y
+                    f(x) = x + z where x: ℝ
+                    where
+                    y: ℝ : a scalar"""
+        func_info = self.gen_func_info(la_str)
+        self.assertEqual(func_info.numpy_func(2).f(3), 5)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    double B = {}(2).f(3);".format(func_info.eig_func_name),
+                     "    return ((B - 5) == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_local_func_use_defined_sym_unorder(self):
+        la_str = """f(x) = x + z where x: ℝ
+                    z = y
+                    where
+                    y: ℝ : a scalar"""
+        func_info = self.gen_func_info(la_str)
+        self.assertEqual(func_info.numpy_func(2).f(3), 5)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    double B = {}(2).f(3);".format(func_info.eig_func_name),
+                     "    return ((B - 5) == 0);",
                      "}"]
         cppyy.cppdef('\n'.join(func_list))
         self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
