6db36ceed668ce4ab0aa50cf94436523c57d42f4
pressureless <190361902@qq.com>
2022-07-06 23:41:32 -0400

    Add header when necessary

--- iheartla/la_parser/ir_visitor.py
+++ iheartla/la_parser/ir_visitor.py
@@ -288,6 +288,7 @@ class IRVisitor(IRBaseVisitor):
         self.ret_symbol = None
         self.unofficial_method = False  # matrix pow only(eigen)
         self.has_opt = False  # whether opt expr exists
+        self.need_mutator = False
         self.content = ''
         self.local_func_def = ''
         self.local_func_syms = []
@@ -537,6 +538,7 @@ class IRVisitor(IRBaseVisitor):
         self.ret_symbol = type_walker.ret_symbol
         self.unofficial_method = type_walker.unofficial_method
         self.has_opt = type_walker.has_opt
+        self.need_mutator = type_walker.need_mutator
         self.lhs_list = type_walker.lhs_list
         self.la_content = type_walker.la_content
         self.local_func_syms = type_walker.local_func_syms
