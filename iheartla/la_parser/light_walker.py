d60e5d1a168575adab96890731345085d7829675
pressureless <190361902@qq.com>
2022-08-09 18:11:29 -0400

    Inherit light walker

--- iheartla/la_parser/light_walker.py
+++ iheartla/la_parser/light_walker.py
@@ -14,7 +14,7 @@ class LightWalker(NodeWalker):
         super().__init__()
 
     def walk_Node(self, node):
-        pass
+        return node.text
 
     def walk_object(self, o):
         pass
@@ -22,31 +22,38 @@ class LightWalker(NodeWalker):
     def walk_Start(self, node, **kwargs):
         for vblock in node.vblock:
             self.walk(vblock, **kwargs)
+        return node.text
 
     def walk_ParamsBlock(self, node, **kwargs):
         self.walk(node.conds, **kwargs)
+        return node.text
 
     def walk_WhereConditions(self, node, **kwargs):
         for i in range(len(node.value)):
             self.walk(node.value[i], **kwargs)
+        return node.text
 
     def walk_WhereCondition(self, node, **kwargs):
         self.walk(node.type, **kwargs)
         for id_index in range(len(node.id)):
             self.walk(node.id[id_index], **kwargs)
+        return node.text
 
     def walk_MatrixType(self, node, **kwargs):
         self.walk(node.id1, **kwargs)
         self.walk(node.id2, **kwargs)
+        return node.text
 
     def walk_VectorType(self, node, **kwargs):
         self.walk(node.id1, **kwargs)
+        return node.text
 
     def walk_ScalarType(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_SetType(self, node, **kwargs):
         self.walk(node.cnt, **kwargs)
+        return node.text
 
     def walk_FunctionType(self, node, **kwargs):
         if node.params:
@@ -54,39 +61,48 @@ class LightWalker(NodeWalker):
                 self.walk(node.params[index], **kwargs)
         for cur_index in range(len(node.ret)):
             self.walk(node.ret[cur_index], **kwargs)
+        return node.text
 
     def walk_Import(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Statements(self, node, **kwargs):
         if node.stats:
             self.walk(node.stats, **kwargs)
+        return node.text
 
     def walk_Expression(self, node, **kwargs):
         self.walk(node.value, **kwargs)
+        return node.text
 
     def walk_Add(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_Subtract(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_AddSub(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_Multiply(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_Divide(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_Subexpression(self, node, **kwargs):
         self.walk(node.value, **kwargs)
+        return node.text
 
     def walk_LocalFunc(self, node, **kwargs):
         self.walk(node.name)
@@ -97,6 +113,7 @@ class LightWalker(NodeWalker):
             self.walk(node.expr[cur_index], **kwargs)
         for index in range(len(node.params)):
             self.walk(node.params[index], **kwargs)
+        return node.text
 
     def walk_Assignment(self, node, **kwargs):
         if node.v:
@@ -107,6 +124,7 @@ class LightWalker(NodeWalker):
             for cur_index in range(len(node.left)):
                 self.walk(node.left[cur_index], **kwargs)
                 self.walk(node.right[cur_index], **kwargs)
+        return node.text
 
     def walk_Summation(self, node, **kwargs):
         if node.cond:
@@ -120,6 +138,7 @@ class LightWalker(NodeWalker):
             else:
                 self.walk(node.sub, **kwargs)
         self.walk(node.exp, **kwargs)
+        return node.text
 
     def walk_Optimize(self, node, **kwargs):
         if len(node.init) > 0:
@@ -131,15 +150,18 @@ class LightWalker(NodeWalker):
         self.walk(node.exp, **kwargs)
         if node.cond:
             self.walk(node.cond, **kwargs)
+        return node.text
 
     def walk_MultiCond(self, node, **kwargs):
         if node.m_cond:
             self.walk(node.m_cond, **kwargs)
         self.walk(node.cond, **kwargs)
+        return node.text
 
     def walk_Domain(self, node, **kwargs):
         self.walk(node.lower, **kwargs)
         self.walk(node.upper, **kwargs)
+        return node.text
 
     def walk_Integral(self, node, **kwargs):
         self.walk(node.id, **kwargs)
@@ -148,6 +170,7 @@ class LightWalker(NodeWalker):
         else:
             self.walk(node.lower, **kwargs)
             self.walk(node.upper, **kwargs)
+        return node.text
 
     def walk_Norm(self, node, **kwargs):
         self.walk(node.value, **kwargs)
@@ -160,47 +183,54 @@ class LightWalker(NodeWalker):
                 self.walk(node.sub, **kwargs)
         if node.power:
             self.walk(node.power, **kwargs)
+        return node.text
 
     def walk_InnerProduct(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
         if node.sub:
             self.walk(node.sub, **kwargs)
+        return node.text
 
     def walk_FroProduct(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_HadamardProduct(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_CrossProduct(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_KroneckerProduct(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_DotProduct(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_Derivative(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Partial(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Divergence(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Gradient(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Laplace(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Power(self, node, **kwargs):
         self.walk(node.base, **kwargs)
@@ -210,16 +240,20 @@ class LightWalker(NodeWalker):
             pass
         else:
             self.walk(node.power, **kwargs)
+        return node.text
 
     def walk_Solver(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_Transpose(self, node, **kwargs):
         self.walk(node.f, **kwargs)
+        return node.text
 
     def walk_Squareroot(self, node, **kwargs):
         self.walk(node.f, **kwargs)
+        return node.text
 
     def walk_Function(self, node, **kwargs):
         if isinstance(node.name, str):
@@ -228,6 +262,7 @@ class LightWalker(NodeWalker):
             self.walk(node.name, **kwargs)
         for index in range(len(node.params)):
             self.walk(node.params[index], **kwargs)
+        return node.text
 
     def walk_IfCondition(self, node, **kwargs):
         if node.single:
@@ -235,6 +270,7 @@ class LightWalker(NodeWalker):
         else:
             self.walk(node.se, **kwargs)
             self.walk(node.other, **kwargs)
+        return node.text
 
     def walk_AndCondition(self, node, **kwargs):
         if node.atom:
@@ -242,55 +278,66 @@ class LightWalker(NodeWalker):
         else:
             self.walk(node.se, **kwargs)
             self.walk(node.other, **kwargs)
+        return node.text
 
     def walk_AtomCondition(self, node, **kwargs):
         if node.p:
             self.walk(node.p, **kwargs)
         else:
             self.walk(node.cond, **kwargs)
+        return node.text
 
     def walk_InCondition(self, node, **kwargs):
         for item in node.left:
             self.walk(item, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_NotInCondition(self, node, **kwargs):
         for item in node.left:
             self.walk(item, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_NeCondition(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_EqCondition(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_GreaterCondition(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_GreaterEqualCondition(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_LessCondition(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_LessEqualCondition(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_IdentifierSubscript(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         for right in node.right:
             if right != '*':
                 self.walk(right, **kwargs)
+        return node.text
 
     def walk_IdentifierAlone(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Factor(self, node, **kwargs):
         if node.id0:
@@ -309,191 +356,236 @@ class LightWalker(NodeWalker):
             self.walk(node.op, **kwargs)
         elif node.c:
             self.walk(node.c, **kwargs)
+        return node.text
 
     def walk_Pi(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_E(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Integer(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_SubInteger(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_SupInteger(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Double(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Fraction(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Mantissa(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Exponent(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_Float(self, node, **kwargs):
-        pass
+        return node.text
 
     def walk_MultiCondExpr(self, node, **kwargs):
         self.walk(node.ifs, **kwargs)
         if node.other:
             self.walk(node.other, **kwargs)
+        return node.text
 
     def walk_MultiIfs(self, node, **kwargs):
         if node.ifs:
             self.walk(node.ifs, **kwargs)
         if node.value:
             self.walk(node.value, **kwargs)
+        return node.text
 
     def walk_SingleIf(self, node, **kwargs):
         self.walk(node.cond, **kwargs)
         self.walk(node.stat, **kwargs)
+        return node.text
 
     def walk_Vector(self, node, **kwargs):
         for exp in node.exp:
             self.walk(exp, **kwargs)
+        return node.text
 
     def walk_Matrix(self, node, **kwargs):
         self.walk(node.value, **kwargs)
+        return node.text
 
     def walk_MatrixRows(self, node, **kwargs):
         if node.rs:
             self.walk(node.rs, **kwargs)
         if node.r:
             self.walk(node.r, **kwargs)
+        return node.text
 
     def walk_MatrixRow(self, node, **kwargs):
         if node.rc:
             self.walk(node.rc, **kwargs)
         if node.exp:
             self.walk(node.exp, **kwargs)
+        return node.text
 
     def walk_MatrixRowCommas(self, node, **kwargs):
         if node.value:
             self.walk(node.value, **kwargs)
         if node.exp:
             self.walk(node.exp, **kwargs)
+        return node.text
 
     def walk_ExpInMatrix(self, node, **kwargs):
         self.walk(node.value, **kwargs)
+        return node.text
 
     def walk_NumMatrix(self, node, **kwargs):
         self.walk(node.id1, **kwargs)
         if node.id2:
             self.walk(node.id2, **kwargs)
+        return node.text
 
     def walk_SinFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_AsinFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_CosFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_AcosFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_TanFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_AtanFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_SinhFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_AsinhFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_CoshFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_AcoshFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_TanhFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_AtanhFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_CotFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_SecFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_CscFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_Atan2Func(self, node, **kwargs):
         self.walk(node.param, **kwargs)
         self.walk(node.second, **kwargs)
+        return node.text
 
     def walk_ExpFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_LogFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_LnFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_SqrtFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_TraceFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_DiagFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_VecFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_DetFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_RankFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_NullFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_OrthFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_InvFunc(self, node, **kwargs):
         self.walk(node.param, **kwargs)
+        return node.text
 
     def walk_ArithExpression(self, node, **kwargs):
         self.walk(node.value, **kwargs)
+        return node.text
 
     def walk_ArithSubexpression(self, node, **kwargs):
         self.walk(node.value, **kwargs)
+        return node.text
 
     def walk_ArithAdd(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_ArithSubtract(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_ArithMultiply(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_ArithDivide(self, node, **kwargs):
         self.walk(node.left, **kwargs)
         self.walk(node.right, **kwargs)
+        return node.text
 
     def walk_ArithFactor(self, node, **kwargs):
         if node.id0:
@@ -506,6 +598,7 @@ class LightWalker(NodeWalker):
             self.walk(node.num, **kwargs)
         elif node.sub:
             self.walk(node.sub, **kwargs)
+        return node.text
 
 
 class SolverParamWalker(LightWalker):
