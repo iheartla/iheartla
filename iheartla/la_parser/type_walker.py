1c91778e8e4904ed7cf62b0a7074bd1a17d514cf
pressureless <190361902@qq.com>
2022-09-26 15:45:51 -0400

    Check matrix dim

--- iheartla/la_parser/type_walker.py
+++ iheartla/la_parser/type_walker.py
@@ -2094,6 +2094,7 @@ class TypeWalker(NodeWalker):
         self.assert_expr(right_info.la_type.is_matrix() or right_info.la_type.is_vector(), get_err_msg_info(left_info.ir.parse_info, "Parameter {} must be a matrix or vector".format(node.right.text)))
         node_type = None
         if left_info.la_type.is_matrix():
+            self.assert_expr(is_same_expr(left_info.la_type.rows, left_info.la_type.cols), get_err_msg_info(left_info.ir.parse_info, "Inverse matrix error. The rows should be the same as the columns"))
             self.assert_expr(is_same_expr(left_info.la_type.rows, right_info.la_type.rows), get_err_msg_info(left_info.ir.parse_info, "Parameters {} and {} should have the same rows".format(node.left.text, node.right.text)))
             if right_info.la_type.is_matrix():
                 node_type = MatrixType(rows=left_info.la_type.cols, cols=right_info.la_type.cols, sparse=left_info.la_type.sparse and right_info.la_type.sparse)
