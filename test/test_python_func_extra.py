a34f134c74cb9474de6e6d2a115bc4bcd797ef77
pressureless <190361902@qq.com>
2022-11-26 22:52:12 -0500

    Test func features

---
+++ test/test_python_func_extra.py
@@ -0,0 +1,112 @@
+import sys
+sys.path.append('./')
+from test.base_python_test import *
+import numpy as np
+import scipy
+from scipy import sparse
+import cppyy
+cppyy.add_include_path(eigen_path)
+
+
+class TestExtraFunction(BasePythonTest):
+    def test_func_sub_params(self):
+        la_str = """a_m(f) = m+f where f,m ∈ ℤ 
+                    c = a(1, 2)
+                    """
+        func_info = self.gen_func_info(la_str)
+        self.assertEqual(func_info.numpy_func().c, 3)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    int B = {}().c;".format(func_info.eig_func_name),
+                     "    return ((B - 3) == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_func_sub_params1(self):
+        la_str = """a_m(f) = m+f where f,m ∈ ℤ 
+                    c = a_1(2)
+                    """
+        func_info = self.gen_func_info(la_str)
+        self.assertEqual(func_info.numpy_func().c, 3)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    int B = {}().c;".format(func_info.eig_func_name),
+                     "    return ((B - 3) == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_func_unicode_sub_params(self):
+        la_str = """aₘ(f) = m+f where f,m ∈ ℤ 
+                    c = a(1, 2)
+                    """
+        func_info = self.gen_func_info(la_str)
+        self.assertEqual(func_info.numpy_func().c, 3)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    int B = {}().c;".format(func_info.eig_func_name),
+                     "    return ((B - 3) == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_func_unicode_sub_params1(self):
+        la_str = """aₘ(f) = m+f where f,m ∈ ℤ 
+                    c = a_1,2
+                    """
+        func_info = self.gen_func_info(la_str)
+        self.assertEqual(func_info.numpy_func().c, 3)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    int B = {}().c;".format(func_info.eig_func_name),
+                     "    return ((B - 3) == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_func_overloading(self):
+        la_str = """aₘ(f) = f + m  where f,m ∈ ℤ 
+                    aₘ(f) = f + m  where f,m ∈ ℝ 
+                    c = a(2, 3)
+                    """
+        func_info = self.gen_func_info(la_str)
+        self.assertEqual(func_info.numpy_func().c, 5)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    int B = {}().c;".format(func_info.eig_func_name),
+                     "    return ((B - 5) == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
+
+    def test_func_overloading_as_param(self):
+        la_str = """a  ∈  ℝ ->  ℝ
+                    a  ∈  ℤ -> ℤ 
+                    b = a(2)
+                    """
+        func_info = self.gen_func_info(la_str)
+        a1 = lambda p: p
+        a2 = lambda p: p
+        self.assertEqual(func_info.numpy_func(a1, a2).b, 2)
+        # eigen test
+        cppyy.include(func_info.eig_file_name)
+        func_list = ["bool {}(){{".format(func_info.eig_test_name),
+                     "    std::function<double>(double)> a1;"
+                     "    a1 = [](double p)->double{"
+                     "    return p;"
+                     "    };",
+                     "    std::function<int>(int)> a2;"
+                     "    a2 = [](int p)->int{"
+                     "    return p;"
+                     "    };",
+                     "    int B = {}(a1, a2).b;".format(func_info.eig_func_name),
+                     "    return ((B - 2) == 0);",
+                     "}"]
+        cppyy.cppdef('\n'.join(func_list))
+        self.assertTrue(getattr(cppyy.gbl, func_info.eig_test_name)())
