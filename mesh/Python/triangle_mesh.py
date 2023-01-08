e11c5752fdc757a923a6242fd8f5b8ee053c40c9
pressureless <190361902@qq.com>
2023-01-07 21:41:10 -0500

    Init Python lib

---
+++ mesh/Python/triangle_mesh.py
@@ -0,0 +1,203 @@
+import numpy as np
+import scipy
+import scipy.linalg
+from scipy import sparse
+from util import *
+
+
+class TriangleMesh:
+    def __init__(self, T):
+        self.V = None
+        self.E = None
+        self.F = None
+        self.T = None
+        self.Vi = None
+        self.Ei = None
+        self.Fi = None
+        self.Ti = None
+        self.bm1 = None
+        self.bm2 = None
+        self.bm3 = None
+        self.pos_bm1 = None
+        self.pos_bm2 = None
+        self.pos_bm3 = None
+        self.map_f = {}
+        self.map_e = {}
+        self.initialize(T)
+
+    def initialize(self, T):
+        self.V = range(np.amax(T) + 1)
+        self.T = preprocess_matrix(T)
+        if T.shape[1] == 4:
+            # tets
+            self.create_faces()
+            self.create_edges()
+            self.build_boundary_mat1()
+            self.build_boundary_mat2()
+            self.build_boundary_mat3()
+        elif T.shape[1] == 3:
+            # faces
+            self.F = T
+            self.create_edges()
+            self.build_boundary_mat1()
+            self.build_boundary_mat2()
+        self.init_mesh_indices()
+
+    def init_mesh_indices(self):
+        self.Vi = set(self.V)
+        self.Ei = set([i for i in range(self.E.shape[0])])
+        self.Fi = set([i for i in range(self.F.shape[0])])
+        self.Ti = set([i for i in range(self.T.shape[0])])
+
+    def create_edges(self):
+        E = np.zeros((3 * self.F.shape[0], 2), dtype=int)
+        for i in range(self.F.shape[0]):
+            E[3 * i, :] = sort_vector([self.F[i, 0], self.F[i, 1]])
+            E[3 * i + 1, :] = sort_vector([self.F[i, 0], self.F[i, 2]])
+            E[3 * i + 2, :] = sort_vector([self.F[i, 1], self.F[i, 2]])
+        self.E = np.asarray(remove_duplicate_rows(sort_matrix(E)), dtype=int)
+        for i in range(self.E.shape[0]):
+            self.map_e[(self.E[i, 0], self.E[i, 1])] = i
+
+    def create_faces(self):
+        F = np.zeros((4*self.T.shape[0], 3), dtype=int)
+        for i in range(self.T.shape[0]):
+            F[4 * i, :] = sort_vector([self.T[i, 0], self.T[i, 1], self.T[i, 2]])
+            F[4 * i + 1, :] = sort_vector([self.T[i, 0], self.T[i, 1], self.T[i, 3]])
+            F[4 * i + 2, :] = sort_vector([self.T[i, 0], self.T[i, 2], self.T[i, 3]])
+            F[4 * i + 3, :] = sort_vector([self.T[i, 1], self.T[i, 2], self.T[i, 3]])
+        self.F = np.asarray(remove_duplicate_rows(sort_matrix(F)), dtype=int)
+        for i in range(self.F.shape[0]):
+            self.map_f[(self.F[i, 0], self.F[i, 1], self.F[i, 2])] = i
+
+    def get_face_index(self, i, j, k):
+        sign = -1
+        p = permute_rvector([i, j, k])
+        key = (p[0], p[1], p[2])
+        if key in self.map_f:
+            sign = 1
+            return self.map_f[key], sign
+        return self.map_f[(p[0], p[2], p[1])], sign
+
+    def get_edge_index(self, i, j):
+        sign = -1
+        if i < j:
+            sign = 1
+            return self.map_e[(i, j)], sign
+        return self.map_e[(j, i)], sign
+
+    def build_boundary_mat1(self):
+        index_list = []
+        value_list = []
+        for i in range(self.E.shape[0]):
+            index_list.append((self.E[i, 0], i))
+            value_list.append(-1)
+            index_list.append((self.E[i, 1], i))
+            value_list.append(1)
+        self.bm1 = scipy.sparse.coo_matrix((value_list, np.asarray(index_list).T), shape=(len(self.V), self.E.shape[0]), dtype=int)
+        self.pos_bm1 = scipy.sparse.coo_matrix(([abs(v) for v in value_list], np.asarray(index_list).T), shape=(len(self.V), self.E.shape[0]), dtype=int)
+
+
+    def build_boundary_mat2(self):
+        index_list = []
+        value_list = []
+        for i in range(self.F.shape[0]):
+            idx, sign = self.get_edge_index(self.F[i, 0], self.F[i, 1])
+            index_list.append((idx, i))
+            value_list.append(sign)
+            idx, sign = self.get_edge_index(self.F[i, 0], self.F[i, 2])
+            index_list.append((idx, i))
+            value_list.append(-sign)
+            idx, sign = self.get_edge_index(self.F[i, 1], self.F[i, 2])
+            index_list.append((idx, i))
+            value_list.append(sign)
+        self.bm2 = scipy.sparse.coo_matrix((value_list, np.asarray(index_list).T), shape=(self.E.shape[0], self.F.shape[0]), dtype=int)
+        self.pos_bm2 = scipy.sparse.coo_matrix(([abs(v) for v in value_list], np.asarray(index_list).T), shape=(self.E.shape[0], self.F.shape[0]), dtype=int)
+
+
+    def build_boundary_mat3(self):
+        index_list = []
+        value_list = []
+        for i in range(self.T.shape[0]):
+            idx, sign = self.get_face_index(self.T[i, 0], self.T[i, 1], self.T[i, 2])
+            index_list.append((idx, i))
+            value_list.append(-sign)
+            idx, sign = self.get_face_index(self.T[i, 0], self.T[i, 1], self.T[i, 3])
+            index_list.append((idx, i))
+            value_list.append(sign)
+            idx, sign = self.get_face_index(self.T[i, 0], self.T[i, 2], self.T[i, 3])
+            index_list.append((idx, i))
+            value_list.append(-sign)
+            idx, sign = self.get_face_index(self.T[i, 1], self.T[i, 2], self.T[i, 3])
+            index_list.append((idx, i))
+            value_list.append(sign)
+        self.bm3 = scipy.sparse.coo_matrix((value_list, np.asarray(index_list).T), shape=(self.F.shape[0], self.T.shape[0]), dtype=int)
+        self.pos_bm3 = scipy.sparse.coo_matrix(([abs(v) for v in value_list], np.asarray(index_list).T), shape=(self.F.shape[0], self.T.shape[0]), dtype=int)
+
+
+    def nonzeros(self, target):
+        return set(target.nonzero()[0])
+
+    def MeshSets(self):
+        return [self.Vi, self.Ei, self.Fi]
+
+    def BoundaryMatrices(self):
+        return [self.bm1, self.bm2]
+
+    def UnsignedBoundaryMatrices(self):
+        return [self.pos_bm1, self.pos_bm2]
+
+    def vertices_to_vector(self, vset):
+        index_list = []
+        value_list = []
+        for v in vset:
+            index_list.append((v, 0))
+            value_list.append(1)
+        return scipy.sparse.coo_matrix((value_list, np.asarray(index_list).T), shape=(len(self.V), 1), dtype=int)
+
+    def edges_to_vector(self, eset):
+        index_list = []
+        value_list = []
+        for e in eset:
+            index_list.append((e, 0))
+            value_list.append(1)
+        return scipy.sparse.coo_matrix((value_list, np.asarray(index_list).T), shape=(self.E.shape[0], 1), dtype=int)
+
+    def faces_to_vector(self, fset):
+        index_list = []
+        value_list = []
+        for f in fset:
+            index_list.append((f, 0))
+            value_list.append(1)
+        return scipy.sparse.coo_matrix((value_list, np.asarray(index_list).T), shape=(self.F.shape[0], 1), dtype=int)
+
+    def tets_to_vector(self, tset):
+        index_list = []
+        value_list = []
+        for t in tset:
+            index_list.append((t, 0))
+            value_list.append(1)
+        return scipy.sparse.coo_matrix((value_list, np.asarray(index_list).T), shape=(self.T.shape[0], 1), dtype=int)
+
+
+if __name__ == '__main__':
+    #       2
+    #     / | \
+    #    0  |  3
+    #     \ | /
+    #       1
+    # v, f = igl.read_triangle_mesh("/Users/pressure/Downloads/sphere.obj")
+    T = np.asarray([[0,1,2,3], [1,2,3,4]])
+    P = np.asarray([[1, 1, 1], [1, 2, 3], [2, 1, 4], [3, 1, 2]])
+    tri = TriangleMesh(T)
+    # print(tri.V)
+    # print(tri.E)
+    # print(tri.F)
+    # print(tri.T)
+    # print(tri.pos_bm1)
+    # print(tri.Ei)
+    # print(tri.Fi)
+    a = tri.bm1 * tri.edges_to_vector({2})
+    # print(a)
+    print(tri.nonzeros(a))
+
---
+++ mesh/Python/util.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+
+def permute_rvector(source):
+    if len(source) > 0:
+        min_index = 0
+        cur_val = source[0]
+        for i in range(1, len(source)):
+            if source[i] < cur_val:
+                cur_val = source[i]
+                min_index = i
+        if min_index != 0:
+            res = np.zeros(len(source))
+            for i in range(len(source)):
+                res[i] = source[(i + min_index) % len(source)]
+            return res
+    return source
+
+
+def compare_vec(fir, sec):
+    less = True
+    for i in range(len(fir)):
+        if fir[i] != sec[i]:
+            if fir[i] > sec[i]:
+                less = False
+            break
+    return less
+
+
+def swap(source, i, j):
+    for k in range(source.shape[1]):
+        tmp = source[i, k]
+        source[i, k] = source[j, k]
+        source[j, k] = tmp
+
+
+def swap_vector(source, i, j):
+    tmp = source[i]
+    source[i] = source[j]
+    source[j] = tmp
+
+
+def partition(source, start, end):
+    pivot = source[end, :]
+    i = start - 1
+    for j in range(start, end):
+        cur = source[j, :]
+        if compare_vec(cur, pivot):
+            i = i + 1
+            swap(source, i, j)
+    swap(source, i + 1, end)
+    return i + 1
+
+
+def partition_vector(source, start, end):
+    pivot = source[end]
+    i = start - 1
+    for j in range(start, end):
+        if source[j] < pivot:
+            i = i + 1
+            swap_vector(source, i, j)
+    swap_vector(source, i + 1, end)
+    return i + 1
+
+
+def quick_sort(source, start, end):
+    if start >= end:
+        return
+    p = partition(source, start, end)
+    quick_sort(source, start, p - 1)
+    quick_sort(source, p + 1, end)
+
+
+def quick_sort_vector(source, start, end):
+    if start >= end:
+        return
+    p = partition_vector(source, start, end)
+    quick_sort_vector(source, start, p - 1)
+    quick_sort_vector(source, p + 1, end)
+
+
+def sort_matrix(source):
+    quick_sort(source, 0, source.shape[0] - 1)
+    return source
+
+
+def sort_vector(source):
+    quick_sort_vector(source, 0, len(source) - 1)
+    return source
+
+
+def remove_duplicate_rows(source):
+    if source.shape[0] == 0:
+        return source
+    output = np.zeros(source.shape)
+    cur = source[0, :]
+    output[0, :] = cur
+    cnt = 1
+    for j in range(source.shape[0] - 1):
+        if np.array_equal(cur, source[j + 1, :]):
+            continue
+        else:
+            cur = source[j + 1, :]
+            output[cnt, :] = cur
+            cnt += 1
+    # output.resize((cnt, source.shape[1]))
+    return output[:cnt, :]
+
+
+def preprocess_matrix(source):
+    new_source = np.zeros(source.shape)
+    for i in range(source.shape[0]):
+        new_source[i, :] = permute_rvector(source[i, :])
+    return remove_duplicate_rows(sort_matrix(new_source))
+
+
+if __name__ == '__main__':
+    P = np.asarray([[1, 31, 4], [1, 3, 1], [1, 1, 1], [31, 4, 1]])
+    # P = np.asarray([[1, 31, 1], [1, 3, 1], [1, 1, 1], [31, 1, 1]])
+    print(preprocess_matrix(P))
+    print(sort_vector([3, 2, 1, 1, 3]))
