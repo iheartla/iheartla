e91fb9606d62e15ffc6305e379856b711826bce1
pressureless <190361902@qq.com>
2023-03-08 15:11:33 -0500

    Convert unicode number if possible

--- iheartla/la_tools/la_helper.py
+++ iheartla/la_tools/la_helper.py
@@ -245,18 +245,18 @@ def la_info(msg):
 def get_unicode_number(unicode):
     # 0:\u2070,1:\u00B9,2:\u00B2,3:\u00B3,4-9:[\u2074-\u2079]
     number_dict = {'⁰':0,'¹':1,'²':2, '³':3,'⁴':4,'⁵':5,'⁶':6,'⁷':7,'⁸':8,'⁹':9 }
-    return number_dict[unicode]
+    return number_dict[unicode] if unicode in number_dict else unicode
 
 def get_unicode_sub_number(unicode):
     # 0-9:[\u2080-\u2089]
     number_dict = {'₀':0,'₁':1,'₂':2, '₃':3,'₄':4,'₅':5,'₆':6,'₇':7,'₈':8,'₉':9 }
-    return number_dict[unicode]
+    return number_dict[unicode] if unicode in number_dict else unicode
 
 def get_unicode_fraction(unicode):
     fraction_dict = {'¼':[1,4],'½':[1,2],'¾':[3,4],'⅐':[1,7],'⅑':[1,9],'⅒':[1,10],'⅓':[1,3],'⅔':[2,3],
                    '⅕':[1,5],'⅖':[2,5],'⅗':[3,5],'⅘':[4,5],'⅙':[1,6],'⅚':[5,6],'⅛':[1,8],'⅜':[3,8],'⅝':[5,8],
                    '⅞':[7,8]}
-    return fraction_dict[unicode]
+    return fraction_dict[unicode] if unicode in fraction_dict else unicode
 
 def get_unicode_subscript(unicode):
     subscript_dict = {'ₐ':'a', 'ₑ':'e', 'ₒ':'o', 'ₓ':'x', 'ₕ':'h', 'ₖ':'k', 'ₗ':'l', 'ₘ':'m', 'ₙ':'n', 'ₚ':'p', 'ₛ':'s', 'ₜ':'t', 'ᵢ':'i', 'ⱼ':'j'}
