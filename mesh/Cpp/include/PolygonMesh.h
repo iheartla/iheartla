69dd6d7b300763610c8a4e2df81b5238d5392fc2
pressureless <190361902@qq.com>
2023-04-09 18:39:34 -0400

    Init polygon lib

---
+++ mesh/Cpp/include/PolygonMesh.h
@@ -0,0 +1,40 @@
+#ifndef PolygonMesh_h
+#define PolygonMesh_h
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <map>
+#include "simplicial_set.h"
+#include "Connectivity.h"
+
+class PolygonMesh: public Connectivity {
+public:
+    PolygonMesh();
+    PolygonMesh(std::vector<std::vector<int> > &T);
+    void initialize(std::vector<std::vector<int> > &T);
+    // void initialize(const std::vector<std::vector<int> > &T);
+    void create_edges();
+
+
+    void create_faces();
+    void build_boundary_mat3(); // T -> F, size: |F|x|T|, boundary of tets
+    void build_boundary_mat2(); // F -> E, size: |E|x|F|, boundary of triangles
+    void build_boundary_mat1(); // E -> V, size: |V|x|E|, boundary of edges
+    void build_nonboundary_edges();
+    int get_edge_index(int i, int j); 
+    int get_edge_index(int i, int j, int &sign);
+    int get_face_index(int i, int j, int k, int &sign);
+    int get_face_index(int i, int j, int k); 
+    void init_mesh_indices();
+    //
+    std::tuple<std::vector<int>, std::vector<int>, std::vector<int>> MeshSets() const;
+    std::tuple<Eigen::SparseMatrix<int>, Eigen::SparseMatrix<int> > BoundaryMatrices() const;
+    std::tuple<Eigen::SparseMatrix<int>, Eigen::SparseMatrix<int> > UnsignedBoundaryMatrices() const;
+    // 
+    bool numerical_order;       // whether the indices are stored as numerical order in edges/faces
+
+    std::vector<std::vector<int> > Face;
+    std::map<std::vector<int>, int> map_face;
+    int max_degree;   // max vertices in a face
+};
+
+#endif
---
+++ mesh/Cpp/src/PolygonMesh.cpp
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm> 
+#include <vector>
+#include <algorithm> 
+#include "dec_util.h"
+#include "PolygonMesh.h"
+
+PolygonMesh::PolygonMesh(){
+
+}
+
+PolygonMesh::PolygonMesh(std::vector<std::vector<int> > &T){
+    this->initialize(T);
+}
+
+void PolygonMesh::initialize(std::vector<std::vector<int> > &T){
+	// T is a list of faces
+	this->Face.resize(T.size());
+	int max_vertices = 0;
+	this->max_degree = 0;
+	for (int i = 0; i < T.size(); ++i)
+	{
+		std::vector<int> cur_f(T[i].begin(), T[i].end());
+		std::sort(cur_f.begin(), cur_f.end());
+		this->map_face[cur_f] = i;                // mapping, sorted indices
+		this->Face[i] = permute_vector(T[i]);     // saved faces, permutated indices
+		if (this->max_degree < T[i].size())
+		{
+			this->max_degree = T[i].size();
+		}
+		for (int j = 0; j < T[i].size(); ++j)
+		{
+			if (T[i][j] > max_vertices)
+			{
+				max_vertices = T[i][j];
+			}
+		}
+	}
+	this->num_v = max_vertices + 1;
+	this->create_edges();
+    // boundary mat
+    this->build_boundary_mat1();
+    this->build_boundary_mat2();
+}
+
+void PolygonMesh::create_edges(){
+    Matrix E((this->max_degree+1)*this->Face.size(), 2); // max edges
+    int cnt = 0;
+    for (int i=0; i<this->Face.size(); i++) {
+    	for (int j = 0; j < this->Face[i].size(); ++j)
+    	{
+    		Vector v0(2); v0 << this->Face[i][j], this->Face[i][(j+1)%this->Face[i].size()];
+        	E.row(cnt) = sort_vector(v0);
+        	cnt++;
+    	}
+    }
+    E.conservativeResize(cnt, 2); 
+    // std::cout<<"before this->E:\n"<<E<<std::endl;
+    this->E = remove_duplicate_rows(sort_matrix(E));    
+    // std::cout<<"this->E:\n"<<this->E<<std::endl;
+    // create the mapping
+    for (int i=0; i<this->E.rows(); i++) {
+        this->map_e.insert(std::pair<key_e, int>(std::make_tuple(this->E(i,0), this->E(i,1)), i));
+    }
+}
+
+void PolygonMesh::build_boundary_mat2(){
+    this->bm2.resize(this->E.rows(), this->Face.size());
+    std::vector<Eigen::Triplet<int> > tripletList;
+    tripletList.reserve(this->max_degree * this->F.rows());
+    int sign = 1;
+    // The faces of a triangle +(f0,f1,f2)
+    for(int i=0; i<this->Face.size(); i++){
+    	for (int j = 0; j < this->Face[i].size(); ++j)
+    	{
+    		// +(f0,f1)
+    		int idx = get_edge_index(this->Face[i][j], this->Face[i][(j+1)%this->Face[i].size()], sign);
+        	tripletList.push_back(Eigen::Triplet<int>(idx, i, sign));
+    	}
+    }
+    this->bm2.setFromTriplets(tripletList.begin(), tripletList.end());
+    this->pos_bm2 = this->bm2.cwiseAbs();
+    std::cout<<"this->bm2:\n"<<this->bm2<<std::endl;
+    // std::cout<<"this->pos_bm2:\n"<<this->pos_bm2<<std::endl;
+}
+
+void PolygonMesh::build_boundary_mat1(){
+    this->bm1.resize(this->num_v, this->E.rows());
+    std::vector<Eigen::Triplet<int> > tripletList;
+    tripletList.reserve(2*this->E.rows());
+    for(int i=0; i<this->E.rows(); i++){
+        tripletList.push_back(Eigen::Triplet<int>(this->E(i,0), i, -1));
+        tripletList.push_back(Eigen::Triplet<int>(this->E(i,1), i, 1));
+    }
+    this->bm1.setFromTriplets(tripletList.begin(), tripletList.end());
+    this->pos_bm1 = this->bm1.cwiseAbs();
+    std::cout<<"this->bm1:\n"<<this->bm1<<std::endl;
+    // std::cout<<"this->pos_bm1:\n"<<this->pos_bm1<<std::endl;
+}
+
+int PolygonMesh::get_edge_index(int i, int j, int &sign){
+    if (i < j)
+    {
+        sign = 1;
+        return this->map_e[std::make_tuple(i, j)];
+    }
+    sign = -1;
+    return this->map_e[std::make_tuple(j, i)];
+}
+
+int PolygonMesh::get_edge_index(int i, int j){
+    if (i < j)
+    {
+        return this->map_e[std::make_tuple(i, j)];
+    }
+    return this->map_e[std::make_tuple(j, i)];
+} 
+  
+std::tuple<std::vector<int>, std::vector<int>, std::vector<int>> PolygonMesh::MeshSets() const{
+    return std::tuple<std::vector<int>, std::vector<int>, std::vector<int>>(this->Vi, this->Ei, this->Fi);
+}
+
+std::tuple<Eigen::SparseMatrix<int>, Eigen::SparseMatrix<int> > PolygonMesh::BoundaryMatrices() const{
+    return std::tuple< Eigen::SparseMatrix<int>, Eigen::SparseMatrix<int> >(this->bm1, this->bm2);
+}
+
+std::tuple<Eigen::SparseMatrix<int>, Eigen::SparseMatrix<int> > PolygonMesh::UnsignedBoundaryMatrices() const{
+    return std::tuple< Eigen::SparseMatrix<int>, Eigen::SparseMatrix<int> >(this->pos_bm1, this->pos_bm2);
+}
