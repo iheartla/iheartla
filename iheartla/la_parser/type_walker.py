e28839fc3566b5e43470807d3c759e69572b1c25
pressureless <190361902@qq.com>
2023-04-11 16:19:46 -0400

    Differentiate visualized pdf

--- iheartla/la_parser/type_walker.py
+++ iheartla/la_parser/type_walker.py
@@ -674,7 +674,7 @@ class TypeWalker(NodeWalker):
         self.pre_walk = True if 'pre_walk' in kwargs else False
         self.scope_list = ['global']
         # self.symtable.clear()
-        self.visualizer.visualize(node)  # visualize
+        self.visualizer.visualize(node, self.pre_walk)  # visualize
         ir_node = StartNode(parse_info=node.parseinfo, raw_text=node.text)
         # if node.directive:
         #     for directive in node.directive:
